b2429be2e4a6c5af77aed13caa805b216e570d2b
alexreyes091 <alexreyes09@gmail.com>
2023-05-13 23:08:15 -0600

    Update classes SII

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 from SII.UnidadFomento import UnidadFomento
 from SII.ScrapingSII import ScrapingSII
+from SII.DateSII import DateSII
+
 
 def main():
     uf = UnidadFomento()
-    uf.date = [2022, 12, 8]
-    url = uf.getUrl()
+    uf.date = [2023, 2, 2]
+    url = uf.getUrl('uf')
     
     scrap = ScrapingSII(url)
     # searchBy = scrap.typeSearch['month']
-    data = scrap.getScrap(date=uf.date, typeSearch="month")
-    print(data);
+    data = scrap.getScrap(date=uf.date, typeSearch="day")
+    # print(data);
    
 if __name__ == "__main__":
     main()
