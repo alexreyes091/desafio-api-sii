5eee9ebdc8aa442ba657ecb24bf66389697e0b3e
alexreyes091 <alexreyes09@gmail.com>
2023-05-15 21:24:46 -0600

    file system restructuring update

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 from SII.controller.getDataSII import getDataSII
 
-router = APIRouter(prefix="/api",
+router = APIRouter(prefix="",
                 tags=["Scraping SII"],
                 responses={404: {"description": "Not found in SII"}})
 
-@router.get("/sii/date/{typeSearch}/{unitFoment}/{date}")
+@router.get('/')
+async def root():
+    return RedirectResponse("/docs")
+
+
+@router.get("api/sii/date/{typeSearch}/{unitFoment}/{date}")
 async def dataSII(date: str, typeSearch = str, unitFoment = str):
     return getDataSII(date, typeSearch, unitFoment)
+
--- src/api/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from routes import routes as api
-
-app = FastAPI()
-app.include_router(api.router)
-
